c844b165a5f8e1977c06fd7887078a0921e297f5
yoonsoopark <ypark9@ncsu.edu>
2021-01-22 22:16:04 -0500

    finish the bmi app flow logic

--- BMI Calculator/Controllers/ResultViewController.swift
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -19,7 +19,7 @@ class ResultViewController: UIViewController {
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
-        
+        self.dismiss(animated: true, completion: nil)
     }
     
     /*
